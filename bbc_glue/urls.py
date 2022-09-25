eaa38a399f8cc5f7759bc66e309e23d8fd7f3e4c
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2022-09-25 19:48:10 -0300

    Commit app

--- bbc_glue/urls.py
+++ bbc_glue/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
-    path('bbc_1', views.home_1, name='home_1'),
+    path('home_1/', views.home_1, name='home_1'),
+    path('home_2/', views.home_2, name='home_2'),
+    path('home_3/', views.home_3, name='home_3'),
+    path('home_4/', views.home_4, name='home_4'),
+    # path('home_5/', views.home_5, name='home_5'),
+    # path('home_6/', views.home_6, name='home_6'),
 ]
--- bbc_glue/views.py
+++ bbc_glue/views.py
@@ -5,22 +5,20 @@ from bbc_glue.data_client import *
 def index(request):
     if request.method == 'POST':
         customer_id = request.POST.get('customer_id')
-        if customer_id not in ['427.282.572-59', '595.080.896-84']:
-            return redirect('index')
-        else:
-            if customer_id == '427.282.572-59':
-                pfm_da_valentina = PFM(customerId='427.282.572-59', customer_data=other_customer_data, initialInvestment=0)
-                ip_da_valentina = InitPayment(pfm_da_valentina)
-                credit_engine_da_valentina = CreditEngine(pfm_da_valentina)
-                all_accounts = pfm_da_valentina.allMyAccounts
-                return render(request, 'glue/home.html', {'banks': all_accounts, 'investment': pfm_da_valentina.investment})
-                
-            if customer_id == '595.080.896-84':
-                pfm_da_valentina = PFM(customerId='595.080.896-84', customer_data=customer_data, initialInvestment=0)
-                ip_da_valentina = InitPayment(pfm_da_valentina)
-                credit_engine_da_valentina = CreditEngine(pfm_da_valentina)
-                all_accounts = pfm_da_valentina.allMyAccounts
+        senha = request.POST.get('senha')
+        if customer_id == '595.080.896-84':
+            if senha == '1':
                 return redirect('home')
+            elif senha == '2':
+                return redirect('home_2')
+        elif customer_id == '427.282.572-59':
+            if senha == '1':
+                return redirect('home_4')
+            elif senha == '2':
+                return redirect('home_6')
+            return redirect('home_1')
+        else:
+            return redirect('index')
     else:
         return render(request, 'glue/index.html')
 
@@ -31,8 +29,25 @@ def home(request):
     return render(request, 'glue/home.html', {'banks': pfm_da_valentina.allMyAccounts, 'investment': pfm_da_valentina.investment})
 
 def home_1(request):
-    pfm_da_valentina = PFM(customerId='595.080.896-84')
+    pfm_da_valentina = PFM(customerId='595.080.896-84', customer_data=customer_data, initialInvestment=0)
     ip_da_valentina = InitPayment(pfm=pfm_da_valentina)
     credit_engine_da_valentina = CreditEngine(pfm=pfm_da_valentina)
     ip_da_valentina.toRealocateAccounts()
     return render(request, 'glue/bbc_1.html', {'banks': pfm_da_valentina.allMyAccounts, 'investment': pfm_da_valentina.investment})
+
+def home_2(request):
+    pfm_da_valentina = PFM(customerId='595.080.896-84', customer_data=customer_data, initialInvestment=0)
+    ip_da_valentina = InitPayment(pfm_da_valentina)
+    credit_engine_da_valentina = CreditEngine(pfm_da_valentina)
+    ip_da_valentina.toRealocateAccounts()
+    return render(request, 'glue/home_2.html', {'banks': pfm_da_valentina.allMyAccounts, 'investment': pfm_da_valentina.investment})
+
+def home_3(request):
+    pfm_da_valentina = PFM(customerId='595.080.896-84', customer_data=customer_data, initialInvestment=0)
+    ip_da_valentina = InitPayment(pfm_da_valentina)
+    credit_engine_da_valentina = CreditEngine(pfm_da_valentina)
+    ip_da_valentina.toApplication()
+    return render(request, 'glue/bcc_3.html', {'banks': pfm_da_valentina.allMyAccounts, 'investment': pfm_da_valentina.investment})
+
+def home_4(request):
+    pass
